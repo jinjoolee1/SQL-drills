7710daaec54aabb4d80f213b52257f21577cc6de
jinjoolee1 <jinjoolee126@gmail.com>
2023-05-08 15:22:32 -0500

    delete.sql finished

--- sql-intro/delete.sql
+++ sql-intro/delete.sql
@@ -16,3 +16,19 @@ VALUES ('Leo', 'lion', 12),
 ('Nala', 'lion', 2),
 ('Marie', 'cat', 1),
 ('Flounder', 'fish', 8);
+
+SELECT * FROM animals;
+
+DELETE 
+FROM animals
+WHERE type = 'lion';
+
+
+DELETE 
+FROM animals
+WHERE name LIKE 'M%';
+
+
+DELETE 
+FROM animals
+WHERE age < 9;
